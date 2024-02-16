58026e80d6dcc904ef66030ea6f8589e1ee2c37c
adam wadin <adamwadin@live.se>
2024-02-16 15:57:43 +0100

    ändringar

---
+++ src/Components/Main/Contact.tsx
@@ -0,0 +1,43 @@
+import "../../Styles/Main/Contact.css";
+
+export function About() {
+  return (
+
+    
+    <div className="main-container">
+      <div className="mobile-contact">
+        <span className="about-us">About us</span>
+        <span className="ekerowoken-fresh-food">
+          Ekerowoken Fresh Food bygger på mat. <br />
+          Vi erbjuder ett alternativ till traditionell fastfood som går lika
+          snabbt men som smakar bättre och framförallt är mer hälsosamt.
+        </span>
+       
+        <div className="emy-xobyibymx-unsplash" />
+
+        <div className="rectangle"/>
+        <span className="span-torsgatagan-stockholm">
+          Götgatan 68
+          <br />
+          118 30 Stockholm <br />
+          073 - 788 03 34
+          <br />
+          lak.lakrestaurants@info.com
+          <br />
+          <br />
+        </span>
+        <div className="group">
+          <a
+            href="https://www.google.se/maps/dir//G%C3%B6tgatan+68,+118+26+Stockholm/@59.3131147,18.0688324,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x465f77f09ab7be01:0x6e101cb5ac0eddb8!2m2!1d18.0736979!2d59.3131121?entry=ttu"
+            className="span-directions"
+            target="_blank"
+          >
+            DIRECTIONS
+          </a>
+          <div className="rectangle-1" />
+        </div>
+      </div>
+      
+    </div>
+  );
+}
