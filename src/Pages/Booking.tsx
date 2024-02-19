7f3958e9e9ebc8e1aec3d80c68cd84786135094d
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-02-19 15:32:43 +0100

    starting on restrictions

--- src/Pages/Booking.tsx
+++ src/Pages/Booking.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import Form from '../Components/Main/Form';
+import React from "react";
+import Form from "../Components/Main/Form";
+
+//--------------------------------------------
+
+//hämta önskat datum och tid från formuläret och sätt i filtrering nedan.
+//Räkna ut antal desiredTables baserat på antal gäster.
+
+//Gör en fetch på alla bokningar
+//filtrera bokningarna på datum och tid som angetts i formulär
+//Räkna samman antal bookedTables i bokningarna
+//availableTables = Maxkapaciteten för bord(hårdkodad) minus bookedTables.
+
+//Jämför desiredTables med availableTables.
+
+//--------------------------------------------
 
 const Booking: React.FC = () => {
-  return (
-    <Form />
-  );
-}
+  return <Form />;
+};
 
 export default Booking;
